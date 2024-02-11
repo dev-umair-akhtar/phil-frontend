3b65100b443afbf7d3685b24be10154e9db5f475
Umair <mairooo@outlook.com>
2024-02-11 22:50:17 +0500

    minor

--- src/components/References/CreateReferences.tsx
+++ src/components/References/CreateReferences.tsx
@@ -1,54 +1,59 @@
 import { Button } from "@mui/material";
 import { Paragraph, patchDocument } from "docx";
 import { TProjectRow } from "../../services/ProjectService";
-
 type TCreateReferencesProps = { project: TProjectRow; templates: string[] };
 
 function CreateReferences({ project, templates }: TCreateReferencesProps) {
     async function getFiles(temps: string[]) {
+        const files: File[] = [];
+
         let index = 0;
+
         for (const url of temps) {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const blob = await fetch(url)
+            const file = await fetch(url)
                 .then((res) => res.blob())
                 .then((blob) => {
-                    const file = new File([blob], "template-" + ++index, {
+                    const _file = new File([blob], "template-" + ++index, {
                         type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
                     });
 
-                    console.log(file);
+                    console.log(_file);
 
-                    patchDocument(file, {
-                        patches: {
-                            reference_title: {
-                                type: "paragraph",
-                                children: [new Paragraph(project.title)],
-                            },
-                            reference_background: {
-                                type: "paragraph",
-                                children: [new Paragraph(project.background)],
-                            },
-                            reference_objective: {
-                                type: "paragraph",
-                                children: [new Paragraph(project.objective)],
-                            },
-                        },
-                    }).then((doc) => {
-                        const blob = new Blob([doc], {
-                            type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                        });
-
-                        const file = new File([blob], "asdf.docx", {
-                            type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                        });
-
-                        console.log(URL.createObjectURL(file), file);
-                    });
+                    return _file;
                 });
 
-            //   const file = new File([blob], "asdf.docx", {
-            //       type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            //   });
+            files.push(file);
+        }
+
+        for (const file of files) {
+            patchDocument(file, {
+                patches: {
+                    reference_title: {
+                        type: "paragraph",
+                        children: [new Paragraph(project.title)],
+                    },
+                    reference_background: {
+                        type: "paragraph",
+                        children: [new Paragraph(project.background)],
+                    },
+                    reference_objective: {
+                        type: "paragraph",
+                        children: [new Paragraph(project.objective)],
+                    },
+                },
+            }).then((doc) => {
+                const blob = new Blob([doc], {
+                    type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                });
+
+                const file = new File([blob], "asdf.docx", {
+                    type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                });
+
+                window.open(URL.createObjectURL(file), "_blank");
+
+                // Navigate({ to: URL.createObjectURL(file) });
+            });
         }
     }
 
